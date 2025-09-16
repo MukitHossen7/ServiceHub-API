f9733132af42e1805826ac34aaed5fb2b03b918d
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-16 10:49:50 +0600

    update business schema

--- src/app/modules/business/business.model.ts
+++ src/app/modules/business/business.model.ts
@@ -16,7 +16,7 @@ const businessSchema = new Schema<IBusiness>(
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     businessName: { type: String, required: true, trim: true },
     businessAddress: { type: businessAddressSchema, required: true },
-    zipCode: { type: String, required: true, trim: true },
+    zipCode: { type: String, required: true, index: true },
     businessPhone: { type: String, required: true, trim: true },
     website: { type: String },
     businessLogo: { type: String },
@@ -30,11 +30,19 @@ const businessSchema = new Schema<IBusiness>(
       default: IStatus.PENDING,
     },
     images: { type: [String], default: [] },
+    isDeleted: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: false },
+    location: {
+      type: { type: String, enum: ["Point"], default: "Point" },
+      coordinates: { type: [Number], default: [0, 0] }, // [lng, lat]
+    },
   },
   {
     timestamps: true,
     versionKey: false,
   }
 );
+
+businessSchema.index({ location: "2dsphere" });
 
 export const Business = model<IBusiness>("Business", businessSchema);
