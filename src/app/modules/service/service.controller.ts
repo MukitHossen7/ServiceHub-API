642c09a80fa87e5b473dc7aaaf87fd120cd84fdc
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-21 14:52:27 +0600

    Implement service management endpoints with validation and authorization

--- src/app/modules/service/service.controller.ts
+++ src/app/modules/service/service.controller.ts
@@ -3,9 +3,11 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
 import { servicesService } from "./service.service";
+import { JwtPayload } from "jsonwebtoken";
 
 const createService = catchAsync(async (req: Request, res: Response) => {
-  const ServerData = await servicesService.createService();
+  const user = req.user as JwtPayload;
+  const ServerData = await servicesService.createService(req.body, user);
   sendResponse(res, {
     statusCode: httpStatus.CREATED,
     success: true,
@@ -14,6 +16,69 @@ const createService = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllServices = catchAsync(async (req: Request, res: Response) => {
+  const result = await servicesService.getAllServices();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All services retrieved successfully",
+    data: result,
+  });
+});
+
+const getCommonServices = catchAsync(async (req: Request, res: Response) => {
+  const result = await servicesService.getCommonServices();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Common service types retrieved successfully",
+    data: result,
+  });
+});
+
+const getSingleService = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await servicesService.getSingleService(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Single service retrieved successfully",
+    data: result,
+  });
+});
+
+const updateService = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = req.user as JwtPayload;
+  const result = await servicesService.updateService(id, req.body, user);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Service updated successfully",
+    data: result,
+  });
+});
+
+const deleteService = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = req.user as JwtPayload;
+  const result = await servicesService.deleteService(id, user);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Service deleted successfully",
+    data: result,
+  });
+});
+
 export const serviceController = {
   createService,
+  getAllServices,
+  getCommonServices,
+  getSingleService,
+  updateService,
+  deleteService,
 };
--- src/app/modules/service/service.routes.ts
+++ src/app/modules/service/service.routes.ts
@@ -1,8 +1,45 @@
 import express from "express";
 import { serviceController } from "./service.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
+import { zodValidateRequest } from "../../middlewares/zodValidateRequest";
+import {
+  createServiceZodSchema,
+  updateServiceZodSchema,
+} from "./service.zod.validation";
 
 const serviceRoute = express.Router();
 
-serviceRoute.post("/", serviceController.createService);
+// POST - create service (Vendor / Admin)
+serviceRoute.post(
+  "/",
+  checkAuth(Role.ADMIN, Role.VENDOR),
+  zodValidateRequest(createServiceZodSchema),
+  serviceController.createService
+);
+
+// GET - list all services (public)
+serviceRoute.get("/", serviceController.getAllServices);
+
+// GET - common service types (public)
+serviceRoute.get("/common", serviceController.getCommonServices);
+
+// GET - single service (public)
+serviceRoute.get("/:id", serviceController.getSingleService);
+
+// PATCH - update service (owner Vendor / Admin)
+serviceRoute.patch(
+  "/:id",
+  checkAuth(Role.VENDOR, Role.ADMIN),
+  zodValidateRequest(updateServiceZodSchema),
+  serviceController.updateService
+);
+
+// DELETE - delete service (owner Vendor / Admin)
+serviceRoute.delete(
+  "/:id",
+  checkAuth(Role.VENDOR, Role.ADMIN),
+  serviceController.deleteService
+);
 
 export default serviceRoute;
